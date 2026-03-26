37df58f8b61c5141b63db989415410e1f8d8f743
Sabesh-7 <sabeshpranith@gmail.com>
2026-03-26 14:00:20 +0530

    Add test profile with H2 in-memory database for CI/CD

--- backend/src/test/java/com/sabes/workflow/rule_engine/RuleEngineApplicationTests.java
+++ backend/src/test/java/com/sabes/workflow/rule_engine/RuleEngineApplicationTests.java
@@ -2,8 +2,10 @@ package com.sabes.workflow.rule_engine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RuleEngineApplicationTests {
 
 	@Test
